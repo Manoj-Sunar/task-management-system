3462e9a2b49340c0040a61beffa69a83edc2c22a
Manoj Sunar <mutipletask@gmail.com>
2025-12-11 20:43:26 +0545

    full deployment

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-// Debug the URL
-console.log('API Base URL:', process.env.NEXT_PUBLIC_API_BASE_URL);
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/api/v1';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://task-management-system-xe25.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
